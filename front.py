cb316ba8cbe20b11697791e2d27e8464965d2721
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-03-03 22:04:17 +0100

    displaying login img from strealmit if no bearer token reaches fastapi endpoint

--- front.py
+++ front.py
@@ -3,13 +3,16 @@ import requests
 
 st.title("Streamlit + FastAPI Example")
 
-
-url = "http://localhost:8000/"
+url = "http://localhost:8000/protected"
 response = requests.get(url)
 
 if response.status_code == 200:
     response.encoding = "utf-8"
     html = response.text
     st.write(html, unsafe_allow_html=True)
+elif response.status_code == 401:
+    image_path = "assets/unauthorized_image.png"
+    image = open(image_path, "rb").read()
+    st.image(image, caption="Unauthorized Access")
 else:
     st.write("Failed to fetch HTML")
