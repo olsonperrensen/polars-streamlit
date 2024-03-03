8b068914442e9040e903e3fac773c4c24653f628
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-03-03 23:19:59 +0100

    st.session_state to the rescue

--- front.py
+++ front.py
@@ -2,6 +2,10 @@ import streamlit as st
 import requests
 import polars as pl
 
+# Initialize session_state to store token
+if "token" not in st.session_state:
+    st.session_state.token = None
+
 # Create a sidebar navigation menu
 page = st.sidebar.radio("Proceed sequentially", ["Log in", "Upload .parquet"])
 
@@ -24,7 +28,7 @@ if page == "Log in":
         )
 
         if auth_response.status_code == 200:
-            token = auth_response.json()["access_token"]
+            st.session_state.token = auth_response.json()["access_token"]
             st.success("Login successful! You can now jump to the uploads section.")
             username_placeholder.text("")
             password_placeholder.text("")
@@ -34,23 +38,26 @@ if page == "Log in":
             image = open(image_path, "rb").read()
             st.image(image, caption="Unauthorized Access")
 elif page == "Upload .parquet":
-    st.info(
-        "You can get .parquet files from places like: `https://huggingface.co/datasets/Qdrant/dbpedia-entities-openai3-text-embedding-3-large-1536-1M/tree/refs%2Fconvert%2Fparquet/default/train`"
-    )
-    uploaded_file = st.file_uploader("Upload your Parquet file", type="parquet")
+    if st.session_state.token:  # Check if token is present
+        st.info(
+            "You can get .parquet files from places like: `https://huggingface.co/datasets/Qdrant/dbpedia-entities-openai3-text-embedding-3-large-1536-1M/tree/refs%2Fconvert%2Fparquet/default/train`"
+        )
+        uploaded_file = st.file_uploader("Upload your Parquet file", type="parquet")
 
-    if uploaded_file is not None:
-        df = pl.read_parquet(uploaded_file)
-        st.write(df.head(10))  # Display the first 10 rows of the dataframe
+        if uploaded_file is not None:
+            df = pl.read_parquet(uploaded_file)
+            st.write(df.head(10))  # Display the first 10 rows of the dataframe
 
-    # DEBUG
-    if False:
-        # Display the protected data after successful login
-        url = "http://localhost:8000/protected"
-        headers = {"Authorization": f"Bearer {token}"}
-        response = requests.get(url, headers=headers)
+        # DEBUG
+        if False:
+            # Display the protected data after successful login
+            url = "http://localhost:8000/protected"
+            headers = {"Authorization": f"Bearer {st.session_state.token}"}
+            response = requests.get(url, headers=headers)
 
-        if response.status_code == 200:
-            response.encoding = "utf-8"
-            html = response.text
-            st.write(html, unsafe_allow_html=True)
+            if response.status_code == 200:
+                response.encoding = "utf-8"
+                html = response.text
+                st.write(html, unsafe_allow_html=True)
+    else:
+        st.warning("Please log in to upload a Parquet file.")
