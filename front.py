0c9ba040b20209c8526eb0459fa2d0277e01a0d0
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-03-03 22:15:49 +0100

    early adoption of login sys

--- front.py
+++ front.py
@@ -1,33 +1,33 @@
 import streamlit as st
 import requests
 
-st.title("Streamlit + FastAPI Example")
+st.title("Login Page")
 
-# Authentication endpoint to obtain JWT token
-auth_url = "http://localhost:8000/token"
-auth_response = requests.post(
-    auth_url, data={"username": "user1", "password": "password1"}
-)
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+login_button = st.button("Login")
+if login_button:
+    # Authentication endpoint to obtain JWT token
+    auth_url = "http://localhost:8000/token"
+    auth_response = requests.post(
+        auth_url, data={"username": username, "password": password}
+    )
 
-if auth_response.status_code == 200:
-    token = auth_response.json()["access_token"]
+    if auth_response.status_code == 200:
+        token = auth_response.json()["access_token"]
+        st.success("Login successful! You can now access the protected data.")
+        st.write("Redirecting to the protected data...")
 
-    # Access protected route with JWT token
-    url = "http://localhost:8000/protected"
-    headers = {"Authorization": f"Bearer {token}"}
-    response = requests.get(url, headers=headers)
+        # Display the protected data after successful login
+        url = "http://localhost:8000/protected"
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        response.encoding = "utf-8"
-        html = response.text
-        st.write(html, unsafe_allow_html=True)
-    elif response.status_code == 401:
+        if response.status_code == 200:
+            response.encoding = "utf-8"
+            html = response.text
+            st.write(html, unsafe_allow_html=True)
+    elif auth_response.status_code == 401:
         image_path = "assets/unauthorized_image.png"
         image = open(image_path, "rb").read()
         st.image(image, caption="Unauthorized Access")
-    else:
-        st.write("Failed to fetch HTML")
-elif auth_response.status_code == 401:
-    image_path = "assets/unauthorized_image.png"
-    image = open(image_path, "rb").read()
-    st.image(image, caption="Unauthorized Access")
