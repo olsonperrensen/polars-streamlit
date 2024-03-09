8f1148dc18cdf1d3c330de327799bbfc4874bdaf
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-03-09 17:50:41 +0100

    clean code refactoring

---
+++ pages/explore.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import polars as pl
+
+
+# Function to render the Polars table
+def render_table(start_row=0, end_row=1, start_col=0, end_col=1):
+    data = pl.scan_parquet("data.parquet", n_rows=end_row)
+    data = data.collect()
+    df = pl.DataFrame(data)
+    df = df.select(pl.col(df.columns[start_col:end_col]))
+    df = df.slice(start_row, end_row - start_row)
+    st.dataframe(df, use_container_width=True)
+
+
+if st.session_state.token:  # Check if token is present
+    # Sliders for choosing the number of rows and columns
+    start_row, end_row = st.slider("Rows", 0, 222, (0, 10))
+    start_col, end_col = st.slider("Columns", 0, 5, (0, 2))
+
+    # Initialize button states
+    up, down, left, right = st.columns([1, 1, 1, 1])
+
+    with up:
+        st.button("⬆️")
+    with down:
+        st.button("⬇️")
+    with left:
+        st.button("⬅️")
+    with right:
+        st.button("➡️")
+
+    render_table(start_row, end_row, start_col, end_col)
+
+else:
+    st.warning("Please log in")
