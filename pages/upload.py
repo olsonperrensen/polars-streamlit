c2fbf50b103ebc949527f328294013b7e3dbeba6
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-03-09 18:11:33 +0100

    upload endpoint protected by login

--- pages/upload.py
+++ pages/upload.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import requests
 
-send = st.button("send")
+if st.session_state.token:  # Check if token is present
+    send = st.button("send")
 
-if send:
-    with open("data.parquet", "rb") as f:
-        r = requests.post(
-            "http://localhost:8000/protected",
-            headers={"Authorization": f"Bearer {st.session_state.token}"},
-            files={"file": f},
-        )
-        if r.status_code == 200:
-            st.write("Done.")
-            r.encoding = "utf-8"
-            res = r.text
-            st.write(res)
-        else:
-            st.write("Could not send file.")
+    if send:
+        with open("data.parquet", "rb") as f:
+            r = requests.post(
+                "http://localhost:8000/protected",
+                headers={"Authorization": f"Bearer {st.session_state.token}"},
+                files={"file": f},
+            )
+            if r.status_code == 200:
+                st.write("Done.")
+                r.encoding = "utf-8"
+                res = r.text
+                st.write(res)
+            else:
+                st.write(f"An error occured. Error: {r.status_code} - {r.text}")
+else:
+    st.warning("Please log in")
