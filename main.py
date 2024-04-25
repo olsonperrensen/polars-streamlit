39da8340e69100732346516e70daf224dd22da6d
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-04-25 23:10:34 +0200

    sandbox init

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import sys
+from io import StringIO
+
+app = FastAPI()
+
+
+class CodeBody(BaseModel):
+    python_code: str
+
+
+@app.post("/execute_python")
+def execute_python_code(code_body: CodeBody):
+    user_namespace = {}
+    sys.stdout = stdout_buffer = StringIO()
+    exec(code_body.python_code, user_namespace)
+    sys.stdout = sys.__stdout__
+    return {
+        "output": stdout_buffer.getvalue(),
+        "result": user_namespace.get("result", None),
+    }
--- pages/llm.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import random
-import time
-
-st.write(st.query_params.get_all())
