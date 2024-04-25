cbf446f1af9a973e2f21032448cfc8e94709b454
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-04-25 23:27:40 +0200

    simplest imports needed in sandbox

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class CodeBody(BaseModel):
     python_code: str
 
 
-@app.post("/execute_python")
+@app.post("/")
 def execute_python_code(code_body: CodeBody):
     user_namespace = {}
     sys.stdout = stdout_buffer = StringIO()
