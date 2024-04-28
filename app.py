a288c2601831ae1a668e610b83311ca3620f984a
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-04-28 16:02:25 +0200

    better presenting text

--- app.py
+++ app.py
@@ -33,89 +33,58 @@ show_pages(
         Page("pages/Query.py", "Query", "📅"),
     ]
 )
-st.title("🌟 Welcome to PolarSpace! 🚀")
+st.title("🌟 Welcome to PolarSpace! 🌌")
 
 st.write(
     "PolarSpace is a powerful and intuitive web application that empowers users to explore, analyze, and visualize data using the Polars library and Streamlit framework. 📊📈"
 )
-
 feature_tabs = st.tabs(
     ["Data Interaction", "Analysis & Visualization", "Security & Performance"]
 )
 
 with feature_tabs[0]:
-    st.header("📁 Data Interaction")
-    data_interaction_options = st.radio(
-        "Select a data interaction feature:",
-        [
-            "Seamless Data Uploading",
-            "Dynamic Data Querying",
-            "Code Snippets and Examples",
-        ],
-    )
-
-    if data_interaction_options == "Seamless Data Uploading":
+    with st.expander("📁 Seamless Data Uploading"):
         st.write(
-            "📁 Seamless Data Uploading: Users can effortlessly target their Parquet files (up to 2GB in size). The app automatically handles file validation and provides informative error messages for a smooth user experience."
+            "Users can effortlessly target their Parquet files (up to 2GB in size). The app automatically handles file validation and provides informative error messages for a smooth user experience."
         )
-    elif data_interaction_options == "Dynamic Data Querying":
+    with st.expander("🎛️ Dynamic Data Querying"):
         st.write(
-            "🎛️ Dynamic Data Querying: PolarSpace offers a highly interactive and customizable data experience. Users can dynamically select columns, slice rows, and apply Polars expressions to explore and transform their data."
+            "PolarSpace offers a highly interactive and customizable data experience. Users can dynamically select columns, slice rows, and apply Polars expressions to explore and transform their data."
         )
-    else:
+    with st.expander("📝 Code Snippets and Examples"):
         st.write(
-            "📝 Code Snippets and Examples: PolarSpace provides a collection of pre-defined code snippets and examples, making it easy for users to explore and learn the capabilities of Polars and Streamlit."
+            "PolarSpace provides a collection of pre-defined code snippets and examples, making it easy for users to explore and learn the capabilities of Polars and Streamlit."
         )
 
 with feature_tabs[1]:
-    st.header("📊 Analysis & Visualization")
-    analysis_visualization_options = st.radio(
-        "Select an analysis and visualization feature:",
-        [
-            "Interactive Plotting",
-            "High-Performance Data Processing",
-            "Expression History",
-        ],
-    )
-
-    if analysis_visualization_options == "Interactive Plotting":
+    with st.expander("📊 Interactive Plotting"):
         st.write(
-            "📊 Interactive Plotting: Dive deep into your data with interactive plotting capabilities powered by Altair and Plotly. Create stunning 2D and 3D visualizations to gain valuable insights from your datasets."
+            "Dive deep into your data with interactive plotting capabilities powered by Altair and Plotly. Create stunning 2D and 3D visualizations to gain valuable insights from your datasets."
         )
-    elif analysis_visualization_options == "High-Performance Data Processing":
+    with st.expander("⚡ High-Performance Data Processing"):
         st.write(
-            "⚡ High-Performance Data Processing: PolarSpace leverages the power of Polars and optimized data processing techniques to handle large datasets efficiently. Experience lightning-fast data loading, filtering, and aggregation."
+            "PolarSpace leverages the power of Polars and optimized data processing techniques to handle large datasets efficiently. Experience lightning-fast data loading, filtering, and aggregation."
         )
-    else:
+    with st.expander("📜 Expression History"):
         st.write(
-            "📜 Expression History: PolarSpace maintains a history of user expressions and data frame transformations, allowing users to easily track and reproduce their analysis steps."
+            "PolarSpace maintains a history of user expressions and data frame transformations, allowing users to easily track and reproduce their analysis steps."
         )
 
 with feature_tabs[2]:
-    st.header("🔒 Security & Performance")
-    security_performance_options = st.radio(
-        "Select a security and performance feature:",
-        [
-            "Secure Authentication",
-            "Enhanced Security Measures",
-            "Scalable Cloud Infrastructure",
-        ],
+    with st.expander("🔒 Secure Authentication"):
+        st.write(
+            "PolarSpace ensures data privacy and security by implementing a robust authentication system using streamlit-login-auth-ui. Only authenticated users can access the app's features and their data."
+        )
+    with st.expander("🐍 Enhanced Security Measures"):
+        st.write(
+            "The codebase undergoes refactoring, static analysis, and dynamic code analysis to identify and mitigate potential security vulnerabilities. User-provided code is executed in isolated containers, ensuring a secure user experience."
+        )
+    with st.expander("☁️ Scalable Cloud Infrastructure"):
+        st.write(
+            "The app leverages cloud services for scalable remote code execution and data storage, ensuring optimal performance and reliability as the user base grows."
+        )
+
+with st.expander("🎓A final word"):
+    st.write(
+        "PolarSpace is continuously evolving to provide an even better user experience. Get ready to unleash the power of data analysis and visualization with PolarSpace. Happy data-ing! 🌠💫"
     )
-
-    if security_performance_options == "Secure Authentication":
-        st.write(
-            "🔒 Secure Authentication: PolarSpace ensures data privacy and security by implementing a robust authentication system using streamlit-login-auth-ui. Only authenticated users can access the app's features and their data."
-        )
-    elif security_performance_options == "Enhanced Security Measures":
-        st.write(
-            "🐍 Enhanced Security Measures: The codebase undergoes refactoring, static analysis, and dynamic code analysis to identify and mitigate potential security vulnerabilities. User-provided code is executed in isolated containers, ensuring a secure user experience."
-        )
-    else:
-        st.write(
-            "☁️ Scalable Cloud Infrastructure: The app leverages cloud services for scalable remote code execution and data storage, ensuring optimal performance and reliability as the user base grows."
-        )
-
-st.subheader("A final word")
-st.write(
-    "PolarSpace is continuously evolving to provide an even better user experience. Get ready to unleash the power of data analysis and visualization with PolarSpace. Happy data-ing! 🌠💫"
-)
