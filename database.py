59d985322dd4a4814106262761b5ee1037821f77
Max Iturria Cardozo <r0834721@student.thomasmore.be>
2024-05-02 16:22:15 +0200

    fix clashing env

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+NEU_DATABASE_URL = os.getenv("NEU_DATABASE_URL")
+engine = create_engine(NEU_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
